22c3cdd55272b4761c61be19dc0de4cca269fdc2
Jack <primes_unit.05@icloud.com>
2025-08-02 13:24:27 +1200

    vis.gl/react-google-maps with the new gmp-map-3d web component layered in manually.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,24 @@
 import MainMap from "./components/Map";
 
+// function App() {
+//   return (
+//     <div className="flex flex-col p-10 justify-center items-center h-screen">
+//       <div>
+//         <MainMap />
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import ThreeDApp from "./components/React3DMap.tsx";
+
 function App() {
   return (
-    <div className="flex flex-col p-10 justify-center items-center h-screen">
-      <div>
-        <MainMap />
-      </div>
+    <div className="flex flex-col p-4 justify-center items-center h-[100vh]">
+      <MainMap />
+      <ThreeDApp />
     </div>
   );
 }
