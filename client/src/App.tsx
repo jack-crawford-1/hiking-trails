139458e2f37334e9d5c12f163c8dbcafa886012f
Jack <primes_unit.05@icloud.com>
2025-08-03 15:04:53 +1200

    styling to get maps showing side by side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -16,9 +16,9 @@ import ThreeDApp from "./components/React3DMap.tsx";
 
 function App() {
   return (
-    <div className="flex flex-col p-4 justify-center items-center h-[100vh]">
-      <MainMap />
+    <div className="flex flex-row justify-center items-center h-[100vh] p-10 bg-black">
       <ThreeDApp />
+      <MainMap />
     </div>
   );
 }
